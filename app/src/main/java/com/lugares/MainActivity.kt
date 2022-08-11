dd8f8698ebaf533234fe98a12f02503b93b8b655
Christopher Arce <arcecris123@gmail.com>
2022-08-11 10:25:40 -0600

    Clase 11 Finalizada

--- app/src/main/java/com/lugares/MainActivity.kt
+++ app/src/main/java/com/lugares/MainActivity.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.lugares.databinding.ActivityMainBinding
 import java.security.Principal
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val RC_SIGN_IN=9001
+    }
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     //Variable para acceder a la autenticacion
     private lateinit var auth: FirebaseAuth
@@ -32,11 +42,51 @@ class MainActivity : AppCompatActivity() {
 
         binding.btRegister.setOnClickListener { haceRegistro() }
         binding.btLogin.setOnClickListener { haceLogin() }
+        val gso =GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient =GoogleSignIn.getClient(this,gso)
+        binding.btGoogle.setOnClickListener{googleSignIn()}
 
 
     }
 
-        private fun haceLogin() {
+    private fun googleSignIn() {
+        val signIntent = googleSignInClient.signInIntent
+        startActivityForResult(signIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful){
+                    actualiza(auth.currentUser)
+
+                }else{
+                    actualiza(null)
+
+                }
+            }
+    }
+    //metodo cuando regresa de hacer atenticacion en google
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode== RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val cuenta = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(cuenta.idToken!!)
+            }catch (e: ApiException){
+
+            }
+        }
+    }
+
+
+    private fun haceLogin() {
             val email = binding.etCorreo.text.toString()
             val clave = binding.etClave.text.toString()
 
